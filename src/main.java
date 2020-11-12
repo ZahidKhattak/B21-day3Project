1debd36d95bad4b0ba2bcdfe7b97b3535c6a8a8c
ZahidKhattak <74119320+ZahidKhattak@users.noreply.github.com>
2020-11-12 11:04:50 -0600

    add nice message

--- src/main.java
+++ src/main.java
@@ -2,5 +2,7 @@ public class main {
     public static void main(String[] args) {
         //write your code here
         System.out.println("Hello last day");
+        // what is going on today?
+        // today is thiursday
     }
 }
